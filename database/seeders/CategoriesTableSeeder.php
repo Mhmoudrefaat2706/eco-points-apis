d5f4ebe9e7f4af0444109cc891a052cf3f117774
MHMD <115650241+281mohamed@users.noreply.github.com>
2025-07-16 01:21:29 +0300

    Fixed the Conflicts

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -4,11 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
     public function run()
     {
+        // Disable foreign key checks temporarily
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
+        // Clear the table
+        Category::truncate();
+        
+        // Re-enable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         $categories = [
             'Metal',
             'Wood',
